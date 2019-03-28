e403a6f2b84c71cdcf65b99aaaa2d8703348bf96
Teddy <joke79@hanyang.ac.kr>
2019-03-28 19:17:26 +0900

    Styling Stack Navigators

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,17 +1,23 @@
 
 import React from "react";
 import { Platform } from "react-native";
-import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+import {
+    createBottomTabNavigator,
+    createAppContainer,
+    createStackNavigator
+} from "react-navigation";
+
 import MoviesScreen from "../screens/Movies";
 import TVScreen from "../screens/TV";
 import SearchScreen from "../screens/Search";
 import { BG_COLOR } from "../constants/Colors";
 import TabBarIcon from "../components/TabBarIcon";
+import { createStack } from "./config";
 
 const TabNavigation = createBottomTabNavigator(
     {
         Movie: {
-            screen: MoviesScreen,
+            screen: createStack(MoviesScreen, "Movies"),
             navigationOptions: {
                 tabBarIcon: ({ focused }) => (
                     <TabBarIcon
@@ -22,7 +28,7 @@ const TabNavigation = createBottomTabNavigator(
             }
         },
         TV: {
-            screen: TVScreen,
+            screen: createStack(TVScreen, "TV"),
             navigationOptions: {
                 tabBarIcon: ({ focused }) => (
                     <TabBarIcon
@@ -33,7 +39,7 @@ const TabNavigation = createBottomTabNavigator(
             }
         },
         Search: {
-            screen: SearchScreen,
+            screen: createStack(SearchScreen, "Search"),
             navigationOptions: {
                 tabBarIcon: ({ focused }) => (
                     <TabBarIcon
